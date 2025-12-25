4cb4ce79e9c167aa1c0011c45253faeda721d0f5
ZizahAwaliyah <azizahawaliyahjijah@gmail.com>
2025-12-25 13:55:58 +0800

    feat : update API URL

--- frontend-segmentation/app/page.tsx
+++ frontend-segmentation/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://customer-segmentation-inky.vercel.app//predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
